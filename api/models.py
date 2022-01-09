e9c4f4fb0a34481c0afa61e32601ef2d7d12226b
Piotr Sikora <piotrsikoraaa@gmail.com>
2022-01-09 22:42:24 +0100

    Add default

--- api/models.py
+++ api/models.py
@@ -5,17 +5,17 @@ from django.db import models
 
 class Movie(models.Model):
     id = models.UUIDField
-    title = models.CharField(max_length=30)
-    director = models.CharField(max_length=30)
-    screen_writer = models.CharField(max_length=30)
-    release_year = models.IntegerField()
-    rating = models.FloatField()
-    rating_count =  models.IntegerField()
-    genre = models.CharField(max_length=30)
-    production_country = models.CharField(max_length=30)
+    title = models.CharField(max_length=30, default="")
+    director = models.CharField(max_length=30, default="")
+    screen_writer = models.CharField(max_length=30, default="")
+    release_year = models.IntegerField(default=0)
+    rating = models.FloatField(default=0)
+    rating_count =  models.IntegerField(default=0)
+    genre = models.CharField(max_length=30, default="")
+    production_country = models.CharField(max_length=30, default="")
     box_office = models.IntegerField(default=0)
-    length = models.IntegerField()
-    poster_url = models.CharField(max_length=30)
+    length = models.IntegerField(default=0)
+    poster_url = models.CharField(max_length=30, default="")
 
     def __str__(self):
         return self.title
